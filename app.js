ff7387cce148ae7764ca1e884e55684b0d803a75
sangwooyeom <duatkddn@naver.com>
2016-08-06 22:17:04 +0900

    route to app_client/index.html

--- app.js
+++ app.js
@@ -45,9 +45,12 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'app_client')));
 
-app.use('/', routes);
+
 app.use('/api', routesApi);
-// app.use('/users', users);
+
+app.use(function(req, res) {
+  res.sendFile(path.join(__dirname, 'app_client', 'index.html'));
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
