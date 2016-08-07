403e5168d020f51812a76f40e4a64cacdadf5dac
sangwooyeom <duatkddn@naver.com>
2016-08-07 15:33:07 +0900

    remove # from URLs

--- app_client/app.js
+++ app_client/app.js
@@ -2,7 +2,7 @@
 
   angular.module('loc8rApp', ['ngRoute']);
 
-  function config ($routeProvider) {
+  function config ($routeProvider, $locationProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'home/home.view.html',
@@ -10,10 +10,13 @@
         controllerAs: 'vm'
       })
       .otherwise({redirectTo: '/'});
+      
+    // use the HTML5 History API
+    $locationProvider.html5Mode(true);
   }
 
   angular
     .module('loc8rApp')
-    .config(['$routeProvider', config]);
+    .config(['$routeProvider', '$locationProvider', config]);
 
 })();
