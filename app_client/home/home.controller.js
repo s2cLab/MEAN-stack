6320d79f1836810bd27cd297a32d7cfae8540062
sangwooyeom <duatkddn@naver.com>
2016-08-07 22:01:13 +0900

    add detail page

--- app_client/home/home.controller.js
+++ app_client/home/home.controller.js
@@ -4,8 +4,8 @@
     .module('loc8rApp')
     .controller('homeCtrl', homeCtrl);
 
-  homeCtrl.$inject = ['$scope', 'loc8rData', 'geolocation'];
-  function homeCtrl ($scope, loc8rData, geolocation) {
+  homeCtrl.$inject = ['$scope', 'loc8rData'];
+  function homeCtrl ($scope, loc8rData) {
     var vm = this;
     vm.pageHeader = {
       title: 'Loc8r',
@@ -16,9 +16,9 @@
     };
     vm.message = "Checking your location";
 
-    vm.getData = function (position) {
-      var lat = position.coords.latitude,
-          lng = position.coords.longitude;
+    vm.getData = function () {
+      var lat = 51.378091,
+          lng = -0.7992599;
       vm.message = "Searching for nearby places";
       loc8rData.locationByCoords(lat, lng)
         .success(function(data) {
@@ -29,22 +29,7 @@
         .error(function (e) {
           vm.message = "Sorry, something's gone wrong, please try again later";
         });
-    };
-
-    vm.showError = function (error) {
-      $scope.$apply(function() {
-        vm.message = error.message;
-      });
-    };
-
-    vm.noGeo = function () {
-      $scope.$apply(function() {
-        vm.message = "Geolocation is not supported by this browser.";
-      });
-    };
-
-    geolocation.getPosition(vm.getData,vm.showError,vm.noGeo);
-
+    }();
   }
 
 })();
